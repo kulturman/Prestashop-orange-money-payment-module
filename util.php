413a0e0399762a795636eac5e461f24baefec9c3
Arnaud Bakyono <arnaudbakyono@gmail.com>
2025-08-31 10:03:41 +0000

    Update to handle OM new endpoints and new parameter

--- util.php
+++ util.php
@@ -1,6 +1,8 @@
 <?php
 if(!function_exists('sendOrangeMoneyPayment')) {
     function sendOrangeMoneyPayment($customerNumber, $amount, $otp) {
+        $externalTransactionId = $customerNumber .(int)$amount . rand(1,99999) . Configuration::get('ORANGE_MONEY_MERCHANT_NUMBER');
+        
         $params = '
             <?xml version="1.0" encoding="UTF-8"?>
             <COMMAND>
@@ -15,11 +17,12 @@ if(!function_exists('sendOrangeMoneyPayment')) {
                 <PAYID>12</PAYID>
                 <PAYID2>12</PAYID2>
                 <otp>'.$otp.'</otp>
+                <ext_txn_id>'.$externalTransactionId.'</ext_txn_id>
             </COMMAND>
         ';
         $url = Configuration::get('ORANGE_MONEY_IS_TEST_MODE') == 1
-            ? "https://testom.orange.bf:9008/payment"
-            : "https://apiom.orange.bf:9007/payment";
+            ? "https://testom.orange.bf"
+            : "https://apiom.orange.bf";
         $session = curl_init($url);
         curl_setopt($session, CURLOPT_POSTFIELDS, $params);
         curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
